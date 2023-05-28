2d3ee7f4ae5ae3691b3605039564784a368474fc
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2023-05-28 20:52:07 -0300

    Lição 5 concluída

--- flutter_basics/lib/location_detail.dart
+++ flutter_basics/lib/location_detail.dart
@@ -27,9 +27,38 @@ class LocationDetail extends StatelessWidget {
     _sectionText(locationFact.text)
   ];
 
-  Widget _sectionTitle(String text) => Text(text);
+  Widget _sectionTitle(String text) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(25, 25, 25, 10),
+      child: Text(text,
+        textAlign: TextAlign.left,
+        style: const TextStyle(
+          fontSize: 25,
+          color: Colors.black
+        ),
+      )
+    );
+  }
 
-  Widget _sectionText(String text) => Text(text);
+  Widget _sectionText(String text)  {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(25, 25, 25, 10),
+      child: Text(text,
+        textAlign: TextAlign.left,
+        style: const TextStyle(
+          fontSize: 25,
+          color: Colors.black
+        ),
+      )
+    );
+  }
+
+  List<Widget> _renderBody(BuildContext  context, Location location) {
+    var result = List<Widget>.empty(growable: true);
+    result.add(_bannerImage(location.url, 170));
+    result.addAll(_renderFacts(location));
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,8 +69,15 @@ class LocationDetail extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: _renderFacts(location)
+        children: _renderBody(context, location)
       )
+    );
+  }
+
+  Widget _bannerImage(String url, double height) {
+    return Container(
+      constraints: BoxConstraints.tightFor(height: height),
+      child: Image.network(url, fit: BoxFit.fitWidth)
     );
   }
 }
